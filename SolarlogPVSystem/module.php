994025fbd9ab1bf05afde641dc5ce8eb94237437
Ralf1910 <home@ralf-ehren.de>
2017-11-17 12:30:09 +0100

    Update module.php

--- SolarlogPVSystem/module.php
+++ SolarlogPVSystem/module.php
@@ -20,6 +20,9 @@ class SolarlogPVSystem extends IPSModule {
 		$this->RegisterPropertyString("Password", "");
 		$this->RegisterPropertyString("RemoteDir", "");
 		$this->RegisterPropertyString("LocalDir", "");
+		$this->RegisterPropertyString("WR1-Pac", "");
+		$this->RegisterPropertyString("WR1-DaySum", "");
+				
 		// Updates einstellen
 		$this->RegisterTimer("Update", 600*1000, 'PV_Update($_IPS[\'TARGET\']);');
 	}
@@ -105,65 +108,22 @@ class SolarlogPVSystem extends IPSModule {
 				if (($handle = fopen($csvFile, "r")) !== FALSE) {
 					while (($csvdata = fgetcsv($handle, 0, ";")) !== FALSE) {
 	   		     		$num = count($csvdata);
-			  			if ($csvdata[0] != "#Datum" && $csvdata[0] != "#Date" && $num>=$Constants['Spalten']) {
-			  			   if (strlen($csvdata[0]) == 8) $date_time = DateTime::createFromFormat('d.m.y H:i:s', $csvdata[0]." ".$csvdata[1]);
-			  			   if (strlen($csvdata[0]) == 10) $date_time = DateTime::createFromFormat('d.m.Y H:i:s', $csvdata[0]." ".$csvdata[1]);
-	     				  	$monatsWerte[$row]['time']   = $date_time->getTimestamp();
+			  			if ($csvdata[0] != "#Datum" && $csvdata[0] != "#Date") {
+			  			   	if (strlen($csvdata[0]) == 8) $date_time = DateTime::createFromFormat('d.m.y H:i:s', $csvdata[0]." ".$csvdata[1]);
+			  			   	if (strlen($csvdata[0]) == 10) $date_time = DateTime::createFromFormat('d.m.Y H:i:s', $csvdata[0]." ".$csvdata[1]);
+	     				   	$monatsWerte[$row]['time']   = $date_time->getTimestamp();
 
-							// Array durchiterieren und Daten aus CSV Datei zuweisen
-							foreach ($Columns as $key => $value) {
-								$monatsWerte[$row][$key]	= $csvdata[$value];
-							}
-							if ($anzahlWR > 1) {
-									$monatsWerte[$row]['Pdc'] = 0;
-									$monatsWerte[$row]['Pac'] = 0;
-									$monatsWerte[$row]['DaySum'] = 0;
-									for ($WR=1; $WR<=$anzahlWR; $WR++) {
-										$targetPdc="WR".$WR."-Pdc";
-										$monatsWerte[$row][$targetPdc]=0;
-								   	for ($String=1; $String < $stringsProWR[$WR]; $String++) {
-											$sourcePdc="WR".$WR."-Pdc".$String;
-								      	$monatsWerte[$row][$targetPdc] = $monatsWerte[$row][$targetPdc] + $monatsWerte[$row][$sourcePdc];
-										}
-								   	$sourcePac 	  = "WR".$WR."-Pac";
-								   	$sourceDaySum = "WR".$WR."-DaySum";
-										$monatsWerte[$row]['Pdc']    = $monatsWerte[$row]['Pdc']    + $monatsWerte[$row][$targetPdc];
-										$monatsWerte[$row]['Pac']    = $monatsWerte[$row]['Pac']    + $monatsWerte[$row][$sourcePac];
-									   $monatsWerte[$row]['DaySum'] = $monatsWerte[$row]['DaySum'] + $monatsWerte[$row][$sourceDaySum];
-									}
-								}
-								if ($Niederhof) {
-								   $monatsWerte[$row]['NH1-DaySum'] = $monatsWerte[$row]['WR5-DaySum'] + $monatsWerte[$row]['WR6-DaySum'];
-									$monatsWerte[$row]['NH2-DaySum'] = $monatsWerte[$row]['WR1-DaySum'] + $monatsWerte[$row]['WR2-DaySum'];
-									$monatsWerte[$row]['NH3-DaySum'] = $monatsWerte[$row]['WR3-DaySum'] + $monatsWerte[$row]['WR4-DaySum'];
-								   $monatsWerte[$row]['NH1-Pdc']    = $monatsWerte[$row]['WR5-Pdc']    + $monatsWerte[$row]['WR6-Pdc'];
-									$monatsWerte[$row]['NH2-Pdc']    = $monatsWerte[$row]['WR1-Pdc']    + $monatsWerte[$row]['WR2-Pdc'];
-									$monatsWerte[$row]['NH3-Pdc']    = $monatsWerte[$row]['WR3-Pdc']    + $monatsWerte[$row]['WR4-Pdc'];
-								   $monatsWerte[$row]['NH1-Pac']    = $monatsWerte[$row]['WR5-Pac']    + $monatsWerte[$row]['WR6-Pac'];
-									$monatsWerte[$row]['NH2-Pac']    = $monatsWerte[$row]['WR1-Pac']    + $monatsWerte[$row]['WR2-Pac'];
-									$monatsWerte[$row]['NH3-Pac']    = $monatsWerte[$row]['WR3-Pac']    + $monatsWerte[$row]['WR4-Pac'];
-								}
-								if ($Wessow) {
-								   $monatsWerte[$row]['WS1-DaySum'] = $monatsWerte[$row]['WR1-DaySum'] + $monatsWerte[$row]['WR2-DaySum'];
-									$monatsWerte[$row]['WS2-DaySum'] = $monatsWerte[$row]['WR3-DaySum'] + $monatsWerte[$row]['WR4-DaySum'];
-								   $monatsWerte[$row]['WS1-Pdc']    = $monatsWerte[$row]['WR1-Pdc']    + $monatsWerte[$row]['WR2-Pdc'];
-									$monatsWerte[$row]['WS2-Pdc']    = $monatsWerte[$row]['WR3-Pdc']    + $monatsWerte[$row]['WR4-Pdc'];
-								   $monatsWerte[$row]['WS1-Pac']    = $monatsWerte[$row]['WR1-Pac']    + $monatsWerte[$row]['WR2-Pac'];
-									$monatsWerte[$row]['WS2-Pac']    = $monatsWerte[$row]['WR3-Pac']    + $monatsWerte[$row]['WR4-Pac'];
-								}
-								if ($Lindner) {
-								   $monatsWerte[$row]['Aktueller Bezug'] = $monatsWerte[$row]['Verbrauch-Pac'] - $monatsWerte[$row]['WR1-Pac'];
-								   $monatsWerte[$row]['Eigenverbrauch-DaySum'] = 0;
-								   $monatsWerte[$row]['Einspeisung-DaySum'] = 0;
-								}
-								$row++;
-					 		}  // if
-					 	}  // while
+						   	// Daten aus der CSV in das monatsWerte Array überführen
+							$monatsWerte[$row]['WR1-Pac'] = $csvdata[($this->ReadPropertyInteger("WR1-Pac")];
+							$monatsWerte[$row]['WR1-DaySum'] = $csvdata[($this->ReadPropertyInteger("WR1-DaySum")];										  
+  						    $row++;
+						}  // if
+					 }  // while
 	   		 	fclose($handle);
-	  				}  // if handle
-		 		}  // if file exists
-			}  // for-schleife days
-         IPS_LogMessage($_IPS['SELF'], "Einlesen $csvFile fertig, Zeilen $row");
+	  			}  // if handle
+		 	}  // if file exists
+		}  // for-schleife days
+        IPS_LogMessage($_IPS['SELF'], "Einlesen $csvFile fertig, Zeilen $row");
 
 		/* Gesamtverbrauch zusammenaddieren
 		$aktuellerVerbrauchP 	= 	0;
